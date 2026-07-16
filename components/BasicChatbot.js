b997c6a0db7e78c16abf4bf874b24fa1297c3f7b
Vaughn-Alston <vaughn2code@gmail.com>
2026-07-16 08:47:01 -0700

    template for my repond function

--- components/BasicChatbot.js
+++ components/BasicChatbot.js
@@ -2,6 +2,47 @@ import React, { useState, useCallback, useEffect } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView, Platform } from "react-native";
+import { getChat } from "../utils/getChatGPT";
+
+
+
+//This will will be passed to the getChat function as 
+const prompt = [
+  {
+    role: "system",
+    content:
+    "What is my name"
+  },
+];
+
+
+
+
+
+
+
+async function fetchInitialMessage() {
+  //The getChat function takes in array prompt
+  const response = await getChat(prompt);
+  if (response.error) {
+    console.log("RESPONSE ERROR", response);
+  } else {
+
+
+    //Im assuming this line takes in 
+    const message = response.choices[0].message;
+
+
+    //Also chatgpt message
+    console.log("message: ", message);
+    const content = response.choices[0].message.content;
+
+
+    //This will be the ChatGpt  messege
+    console.log("content: ", content);
+  }
+}
+
 
 const CHATBOT_USER_OBJ = {
   _id: 2,
@@ -14,22 +55,31 @@ export default function BasicChatbot() {
 
   useEffect(() => {
     setMessages([
+
       {
+       
         _id: 1,
         text: "Hello, welcome to simple trivia! Say 'Yes' when you're ready to play!",
         createdAt: new Date(),
         user: CHATBOT_USER_OBJ,
       },
+
+      
     ]);
+
+  
+
   }, []);
 
   const addNewMessage = (newMessages) => {
     setMessages((previousMessages) => {
       // console.log("PREVIOUS MESSAGES:", previousMessages);
-      // console.log("NEW MESSAGE:", newMessages);
+       console.log("NEW MESSAGE:", newMessages);
+
       return GiftedChat.append(previousMessages, newMessages);
     });
   };
+
 
   const addBotMessage = (text) => {
     addNewMessage([
@@ -42,13 +92,38 @@ export default function BasicChatbot() {
     ]);
   };
 
-  const respondToUser = (userMessages) => {
+const respondToUser = async (userMessages) => {
+
     console.log("User message text:", userMessages[0].text);
 
-    // Simple chatbot logic (aka Checkpoint 2 onwards) here!
+    // build your conversation array
 
-    addBotMessage("I am da response!");
-  };
+    // call getChat()
+
+    // get the response
+
+    // update chat history
+
+    // display the bot response
+
+};
+
+
+
+
+
+
+  //Here I want to take the user value and push it into an array of objects
+
+  //In that array of objects I will have a const value that will be user
+
+  
+
+
+
+
+
+
 
   const onSend = useCallback((messages = []) => {
     addNewMessage(messages);
